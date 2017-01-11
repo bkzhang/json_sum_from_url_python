9548364c889150712cade186f670330ca1bd7897
bkzhang <brian.k.zhang@gmail.com>
2017-01-11 12:05:46 -0500

    json sum from url

---
+++ README.md
@@ -0,0 +1,5 @@
+# TO RUN
+
+1. Change variables and url to appropriate names.
+2. Open terminal cd to proper directory.
+3. Run program with $ python2.7 json_sum.py
---
+++ json_sum.py
@@ -0,0 +1,17 @@
+import json
+import urllib2
+
+is_empty = False
+page_number = 1
+total_price = 0
+
+while not is_empty:
+	url = '' # insert url in , for page indicator change page number to {} ie. &page=1 -> &page={} 
+	json_file = urllib2.urlopen(url)
+	data = json.load(json_file)
+	if not data['orders']: # replace orders with proper list variable
+		is_empty = True
+	total_price += sum(float(item['price']) for item in data['orders']) # replace price and data with appropriate variables
+	page_number += 1
+
+print(total_price)
